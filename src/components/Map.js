1c0b8545b109ce3bc1fd84b509199cc9b967fc54
Gamjiae <wldo1020@gmail.com>
2024-05-12 13:24:54 +0900

    충전소 API 가져와 마커 띄우기

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,17 +1,19 @@
 /*global kakao*/ 
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
+import LocationBtn from './LocationBtn';
+import axios from 'axios';
 const { kakao } = window;
 
 function Map() {
-  // 현재 위치 담는 곳
   const [location, setLocation] = useState({
     lat: 37.5031571,
     lng: 126.882408,
   });
-  const [map, setMap] = useState();
-  const [marker, setMarker] = useState();
+  const [map, setMap] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [error, setError] = useState(null);
+  const [stations, setStations] = useState([]);
 
-  // 현재 위치 세부 조정
   var options = {
     enableHighAccuracy: true, // 정확한 위치 요구
     timeout: 5000,            // 최대 대기 시간
@@ -19,7 +21,7 @@ function Map() {
   };
 
   // 현재 위치 가져오기
-  useMemo(() => {
+  useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error, options);
     }
@@ -36,9 +38,8 @@ function Map() {
       console.log("위치 받기 실패");
     }
   }, []);
-
-  // 카카오맵 API 가져오기
-  // 화면에 렌더링
+    
+  // 카카오맵 API 연동 및 렌더링
   useEffect(() => {
     if (location.lat && location.lng) { // 현재 위치가 요휴한 경우에만 맵과 마커 생성
       let container = document.getElementById('map');
@@ -47,20 +48,37 @@ function Map() {
         level: 4,
       };
       const newMap = new kakao.maps.Map(container, options);
+      setMap(newMap);
 
-      // 생성된 맵과 마커를 업데이트
-      setMap(newMap); 
-      setMarker(new kakao.maps.Marker({
-        map: newMap,
-        position: new kakao.maps.LatLng(location.lat, location.lng)
-      }));
+      const bounds = newMap.getBounds();
+      const swLatLng = bounds.getSouthWest(); 
+      const neLatLng = bounds.getNorthEast();
+      axios.get('v1/stations', {
+        params: {
+          bounds: `${swLatLng.getLat()},${swLatLng.getLng()},${neLatLng.getLat()},${neLatLng.getLng()}`
+        }
+      }).then(response => {
+        const newMarkers = response.data.data.map(station => {
+          return new kakao.maps.Marker({
+            map: newMap,
+            position: new kakao.maps.LatLng(station.lat, station.lng)
+          });
+        });
+        setMarkers(newMarkers);
+      }).catch(error => { setError(error); console.log(error); });
     }
-    console.log(location);
   }, [location]);
+
+  const BtnOnClick = () => {
+    map.setCenter(new kakao.maps.LatLng(location.lat, location.lng));
+  }
   
   return (
-  <div id="map" style={{ width: "393px", height: "852px" }}>
-  </div>
+    <>
+      <div id="map" style={{ width: "393px", height: "852px" }}>
+        <LocationBtn onClick={BtnOnClick}/>
+      </div>
+    </>
   )
 }
 
